6c2dc7afcacb5e342bdeb9a336ff33e9fcee13ba
kevimoe <kevin.moe@daimler.com>
2020-06-11 08:59:36 +0800

    :tada: init

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Main = void 0;
+function Main() {
+  console.log("Hi");
+}
+exports.Main = Main;
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export function Main () {
+console.log("Hi")
+}
