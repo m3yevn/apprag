229c24cba822bdd75d473542eb4696042d745c0e
kevimoe <kevin.moe@daimler.com>
2020-06-11 09:51:09 +0800

    update

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Main = void 0;
+var options_1 = require("./constants/options");
 function Main() {
-  console.log("Hi");
+  var options = process.argv[2];
+  switch (options) {
+    case options_1.Options.init:
+      break;
+  }
 }
 exports.Main = Main;
